fd2ae5c0366e470ca195af8c567eb698d825aced
well-that-sucks <55409370+well-that-sucks@users.noreply.github.com>
2021-09-25 21:15:06 +0300

    Add files via upload

---
+++ algorithms.py
@@ -0,0 +1,145 @@
+from collections import deque as queue
+
+class Algoritms:
+
+    def return_min(self, res1, res2):
+        if (res1[0] <= res2[0]):
+            return res1
+        return res2
+
+    def retrieve_shortest_path(self, maze, pos, target, path_matrix):
+        d_row = [-1, 0, 1, 0]
+        d_col = [0, 1, 0, -1]
+        t_matrix = [[0 for element in row] for row in maze]
+        t_matrix[target[1]][target[0]] = path_matrix[target[1]][target[0]]
+        position = target
+
+        while position != pos:
+            for i in range(4):
+                adjx = position[0] + d_row[i]
+                adjy = position[1] + d_col[i]
+                if path_matrix[position[1]][position[0]] == path_matrix[adjy][adjx] + 1:
+                    t_matrix[adjy][adjx] = t_matrix[position[1]][position[0]] - 1
+                    position = (adjx, adjy)
+                    break
+        return t_matrix
+
+    def find_path_dfs(self, maze, pos, target, step, path_matrix, visited):
+        if not(pos[1] in range(len(maze)) and pos[0] in range(len(maze[0]))) or visited[pos[1]][pos[0]]:
+            return (999999, path_matrix)
+        path_matrix[pos[1]][pos[0]] = step
+        if pos == target:
+            return (step, path_matrix)
+        if maze[pos[1]][pos[0]] != '#':
+            visited[pos[1]][pos[0]] = True
+            res = self.find_path_dfs(maze, (pos[0] + 1, pos[1]), target, step + 1, [x[:] for x in path_matrix], visited)
+            if res[0] != 999999:
+                return res
+            res = self.find_path_dfs(maze, (pos[0] - 1, pos[1]), target, step + 1, [x[:] for x in path_matrix], visited)
+            if res[0] != 999999:
+                return res
+            res = self.find_path_dfs(maze, (pos[0], pos[1] + 1), target, step + 1, [x[:] for x in path_matrix], visited)
+            if res[0] != 999999:
+                return res
+            res = self.find_path_dfs(maze, (pos[0], pos[1] - 1), target, step + 1, [x[:] for x in path_matrix], visited)
+            if res[0] != 999999:
+                return res
+            #res = self.find_path_dfs(maze, (pos[0] + 1, pos[1]), target, step + 1, [x[:] for x in path_matrix], visited)
+            #res = self.return_min(res, self.find_path_dfs(maze, (pos[0] - 1, pos[1]), target, step + 1, [x[:] for x in path_matrix], visited))
+            #res = self.return_min(res, self.find_path_dfs(maze, (pos[0], pos[1] + 1), target, step + 1, [x[:] for x in path_matrix], visited))
+            #res = self.return_min(res, self.find_path_dfs(maze, (pos[0], pos[1] - 1), target, step + 1, [x[:] for x in path_matrix], visited))
+            #visited[pos[1]][pos[0]] = False
+            return res
+        return (999999, path_matrix)
+
+    def find_path_bfs(self, maze, pos, target, step, path_matrix, visited):
+        d_row = [-1, 0, 1, 0]
+        d_col = [0, 1, 0, -1]
+        q = queue()
+        q.append((step, pos[0], pos[1]))
+
+        while (len(q) > 0):
+            cell = q.popleft()
+            visited[cell[2]][cell[1]] = True
+            if path_matrix[cell[2]][cell[1]] > cell[0] or path_matrix[cell[2]][cell[1]] == 0:
+                path_matrix[cell[2]][cell[1]] = cell[0]
+            if (cell[1], cell[2]) == target:
+                return cell[0], self.retrieve_shortest_path(maze, pos, target, path_matrix)
+            else:
+                for i in range(4):
+                    adjx = cell[1] + d_row[i]
+                    adjy = cell[2] + d_col[i]
+                    if adjy in range(len(maze)) and adjx in range(len(maze[0])) and not(visited[adjy][adjx]) and maze[adjy][adjx] != '#':
+                        q.append((cell[0] + 1, adjx, adjy))
+
+        return path_matrix[target[1]][target[0]], self.retrieve_shortest_path(maze, pos, target, path_matrix)
+
+    def find_path_ucs(self, maze, pos, target, step, path_matrix, visited):
+        d_row = [-1, 0, 1, 0]
+        d_col = [0, 1, 0, -1]
+
+        queue = []
+        queue.append((step, pos[0], pos[1]))
+
+        while (len(queue) > 0):
+            queue = sorted(queue)
+            cell = queue[0]
+            del queue[0]
+            if not(visited[cell[2]][cell[1]]):
+                visited[cell[2]][cell[1]] = True
+                if path_matrix[cell[2]][cell[1]] > cell[0] or path_matrix[cell[2]][cell[1]] == 0:
+                    path_matrix[cell[2]][cell[1]] = cell[0]
+                if (cell[1], cell[2]) == target:
+                    return cell[0], self.retrieve_shortest_path(maze, pos, target, path_matrix)
+                else:
+                    for i in range(4):
+                        adjx = cell[1] + d_row[i]
+                        adjy = cell[2] + d_col[i]
+                        if adjy in range(len(maze)) and adjx in range(len(maze[0])) and not(visited[adjy][adjx]) and maze[adjy][adjx] != '#':
+                            queue.append((cell[0] + 1, adjx, adjy))
+    
+        return path_matrix[target[1]][target[0]], self.retrieve_shortest_path(maze, pos, target, path_matrix)
+
+    def find_path_astar(self, maze, pos, target, step, path_matrix, visited):
+        d_row = [-1, 0, 1, 0]
+        d_col = [0, 1, 0, -1]
+
+        queue = []
+        queue.append((1, 1, 0, pos[0], pos[1]))
+
+        while (len(queue) > 0):
+            queue = sorted(queue)
+            cell = queue[0]
+            del queue[0]
+            if not(visited[cell[4]][cell[3]]):
+                visited[cell[4]][cell[3]] = True
+                if path_matrix[cell[4]][cell[3]] > cell[1] or path_matrix[cell[4]][cell[3]] == 0:
+                    path_matrix[cell[4]][cell[3]] = cell[1]
+                if (cell[3], cell[4]) == target:
+                    return cell[1], self.retrieve_shortest_path(maze, pos, target, path_matrix)
+                else:
+                    for i in range(4):
+                        adjx = cell[3] + d_row[i]
+                        adjy = cell[4] + d_col[i]
+                        if adjy in range(len(maze)) and adjx in range(len(maze[0])) and not(visited[adjy][adjx]) and maze[adjy][adjx] != '#':
+                            cell_g = cell[1] + 1
+                            cell_h = abs(adjx - target[0]) + abs(adjy - target[1])
+                            queue.append((cell_g + cell_h, cell_g, cell_h, adjx, adjy))
+    
+        return path_matrix[target[1]][target[0]], self.retrieve_shortest_path(maze, pos, target, path_matrix)
+
+    def find_path(self, search_function, maze, starting_pos, ending_pos):
+        res = search_function(maze, starting_pos, ending_pos, 1, [[0 for element in row] for row in maze], [[False for element in row] for row in maze])
+        return res[0], res[1]
+
+    def get_dfs_function(self):
+        return self.find_path_dfs
+    
+    def get_bfs_function(self):
+        return self.find_path_bfs
+
+    def get_ucs_function(self):
+        return self.find_path_ucs
+    
+    def get_astar_function(self):
+        return self.find_path_astar
---
+++ main.py
@@ -0,0 +1,245 @@
+from algorithms import Algoritms
+import pygame
+import random
+import time
+
+pygame.init()
+
+from maze import Maze
+from global_def import *
+
+pygame.display.set_caption('Pacman')
+pygame.display.set_icon(icon)
+
+maze = Maze(levels[0])
+maze.generate_new_level(random_level_name)
+algorithms = Algoritms()
+func_helper = [('DFS', algorithms.get_dfs_function()), ('BFS', algorithms.get_bfs_function()), ('UCS', algorithms.get_ucs_function())]
+func_index = 0
+
+running = True
+clock = pygame.time.Clock()
+reset = True
+lives = 3
+score = 0
+
+while running:
+    if reset:
+        maze.load()
+        pacman, ghosts, coins, boosters = maze.init_entities()
+        sprite_order = 1
+        sprite_interval = SPRITE_CHANGE_INTERVAL
+        bot_interval = BOT_CHANGE_DIRECTION_INTERVAL
+        killer_timer = KILLER_MODE_DURATION
+        blinking_interval = BLINKING_BASE_INTERVAL
+        anim_interval = ANIM_FRAME_DURATION
+        anim_ind = 0
+        rand_helper = [-1, 1]
+        is_killer_mode_active = False
+        has_blinked = False
+        is_anim_being_played = False
+        has_player_lost = False
+        are_all_coins_collected = False
+        reset = False
+
+    while running and not(has_player_lost) and not(are_all_coins_collected):
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_z:
+                    func_index = (func_index + 1) % TRAVERSAL_FUNCTIONS_AMOUNT
+                if event.key == pygame.K_LEFT:
+                    pass
+                    #pacman.set_velocity(-3, 0)
+                if event.key == pygame.K_RIGHT:
+                    pass
+                    #pacman.set_velocity(3, 0)
+                if event.key == pygame.K_UP:
+                    pass
+                    #pacman.set_velocity(0, -3)
+                if event.key == pygame.K_DOWN:
+                    pass
+                    #pacman.set_velocity(0, 3)
+
+        if is_killer_mode_active:
+            killer_timer -= 1
+            if killer_timer < BLINKING_WARNING_THRESHOLD:
+                blinking_interval -= 1
+                if blinking_interval == 0:
+                    blinking_interval = BLINKING_BASE_INTERVAL - (BLINKING_WARNING_THRESHOLD - killer_timer) // BLINKING_BASE_INTERVAL * 2
+                    if blinking_interval < BLINKING_BASE_INTERVAL // 5:
+                        blinking_interval = BLINKING_BASE_INTERVAL // 5
+                    if has_blinked:
+                        apply_ghost_sprites(ghosts, [ghost_killer_mode_sprite])
+                    else:
+                        apply_ghost_sprites(ghosts, [blank_sprite])
+                    has_blinked = not(has_blinked)
+            if killer_timer == 0:
+                is_killer_mode_active = False
+                apply_ghost_sprites(ghosts, ghost_sprites)
+
+        sprite_interval -= 1
+        if (sprite_interval == 0):
+            sprite_interval = SPRITE_CHANGE_INTERVAL
+            sprite_order = sprite_order % 2 + 1
+            if (sprite_order == 1):
+                pacman.set_sprite(pacman_sprite1)
+            else:
+                pacman.set_sprite(pacman_sprite2)
+
+        screen.fill((0, 0, 0))
+
+        are_all_coins_collected = True
+        for coin in coins:
+            if (coin.get_visibility_state()):
+                if pacman.check_entity_collision(coin) and not(is_anim_being_played):
+                    score += coin.get_value()
+                    coin.change_visibility_state()
+                else:
+                    are_all_coins_collected = False
+                    screen.blit(coin.get_sprite(), coin.get_pos())
+
+        if are_all_coins_collected:
+            level_idx += 1
+            if (len(levels) > level_idx):
+                maze.set_level(levels[level_idx])
+
+        for booster in boosters:
+            if (booster.get_visibility_state()):
+                if pacman.check_entity_collision(booster) and not(is_anim_being_played):
+                    is_killer_mode_active = True
+                    killer_timer = KILLER_MODE_DURATION
+                    blinking_interval = BLINKING_BASE_INTERVAL
+                    has_blinked = False
+                    apply_ghost_sprites(ghosts, [ghost_killer_mode_sprite])
+                    booster.change_visibility_state()
+                else:
+                    screen.blit(booster.get_sprite(), booster.get_pos())
+
+        if not(is_anim_being_played):
+            pacman.move(maze)
+
+        bot_interval -= 1
+        time_total = 0
+        color_alg_visual_ind = 0
+        rect_offset_ind = 2
+        
+
+        # Try to solve the problem in one piece w/o second loop(overlapping path over ghosts)
+        for ghost in ghosts:
+            if ghost.get_visibility_state():
+                start_time = time.time()
+                step, path = algorithms.find_path(func_helper[func_index][1], maze.get_level(), maze.coord_to_floored_block_position(ghost.get_pos()), maze.coord_to_floored_block_position(pacman.get_pos()))
+                time_total += time.time() - start_time
+                next_cell = find_next_cell(path)
+                ghost_ppos = ghost.get_pos()
+                # Make it possible for ghost to catch up for player
+                ghost.set_velocity(get_direction(next_cell[0] * SPRITE_SIZE, ghost_ppos[0]) * 3, get_direction(next_cell[1] * SPRITE_SIZE, ghost_ppos[1]) * 3)
+
+                for i in range(len(path)):
+                    for j in range(len(path[i])):
+                        if path[i][j] != 0:
+                            pygame.draw.rect(screen, colors[color_alg_visual_ind], pygame.Rect(j * SPRITE_SIZE + rect_offset_ind, i * SPRITE_SIZE + rect_offset_ind, SPRITE_SIZE - 2 * rect_offset_ind, SPRITE_SIZE - 2 * rect_offset_ind),  2)
+                rect_offset_ind += 2
+                color_alg_visual_ind = (color_alg_visual_ind + 1) % len(colors)
+
+        for ghost in ghosts:
+            if not(ghost.get_visibility_state()):
+                ghost.set_invisibility_timer(ghost.get_invisibility_timer() - 1)
+                if ghost.get_invisibility_timer() == 0:
+                    ghost.change_visibility_state()
+                    ghost.reset_pos()
+            else:
+                #if bot_interval == 0:
+                #    new_velocity_x = (random.randint(0, 2) - 1) * 3
+                #    if new_velocity_x == 0:
+                #        new_velocity_y = rand_helper[random.randint(0, 1)] * 3
+                #    else:
+                #        new_velocity_y = 0
+                #    ghost.set_velocity(new_velocity_x, new_velocity_y)
+                if pacman.check_entity_collision(ghost) and not(is_anim_being_played):
+                    if is_killer_mode_active:
+                       ghost.set_invisibility_timer(RESPAWN_TIME)
+                       ghost.change_visibility_state()
+                       score += EATEN_ENEMY_REWARD
+                    else: 
+                        anim_interval = ANIM_FRAME_DURATION
+                        is_anim_being_played = True
+                        anim_ind = 0
+                if not(is_anim_being_played):
+                    ghost.move(maze)
+                screen.blit(ghost.get_sprite(), ghost.get_pos())
+        if (bot_interval == 0):
+            bot_interval = BOT_CHANGE_DIRECTION_INTERVAL
+
+        if not(is_anim_being_played):
+            screen.blit(pacman.get_directed_sprite(), pacman.get_pos())
+
+        if (is_anim_being_played):
+            anim_interval -= 1
+            if anim_interval == 0:
+                if anim_ind >= len(pacman_death_anim):
+                    is_anim_being_played = False
+                    lives -= 1
+                    if lives == 0:
+                        has_player_lost = True
+                    else:
+                        pacman.reset_pos()
+                else:
+                    anim_ind += 1
+                    anim_interval = ANIM_FRAME_DURATION
+            if anim_ind < len(pacman_death_anim):
+                screen.blit(pacman_death_anim[anim_ind], pacman.get_pos())
+        maze.draw_level()
+
+        score_ins = hud_font.render('Score: ' + str(score), True, (255, 255, 255))
+        lives_ins = hud_font.render('Lives: ' + str(lives), True, (255, 255, 255))
+        level_ins = hud_font.render('Level: ' + str(level_idx + 1) + '/' + str(len(levels)), True, (255, 255, 255))
+        current_alg_ins = hud_font.render('Current algorithm: ' + func_helper[func_index][0], True, (255, 255, 255))
+        alg_exec_time_ins = hud_font.render('Algorithm execution time: ' + str(time_total * 1000)[:6] + 'ms', True, (255, 255, 255))
+        screen.blit(score_ins, (0, 0))
+        screen.blit(lives_ins, (0, 30))
+        screen.blit(level_ins, (0, 60))
+        screen.blit(current_alg_ins, (0, 90))
+        screen.blit(alg_exec_time_ins, (0, 120))
+
+        pygame.display.update()
+
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    reset = True
+                    if has_player_lost:
+                        maze.set_level(levels[0])
+                        level_idx = 0
+                        lives = 3
+                        score = 0
+                    if are_all_coins_collected and len(levels) <= level_idx:
+                        running = False
+
+    if running and has_player_lost:
+        screen.fill((0, 0, 0))
+        gameover_ins = transition_font.render('Game over!', True, (255, 255, 255))
+        end_score_ins = transition_font.render('Score: ' + str(score), True, (255, 255, 255))
+        press_key_ins = transition_font.render('Press SPACEBAR to restart the game', True, (255, 255, 255))
+        screen.blit(gameover_ins, gameover_ins.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 40)))
+        screen.blit(end_score_ins, end_score_ins.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)))
+        screen.blit(press_key_ins, press_key_ins.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 40)))
+        pygame.display.update()
+
+    if running and are_all_coins_collected:
+        screen.fill((0, 0, 0))
+        level_completed_ins = transition_font.render('You have completed the level!', True, (255, 255, 255))
+        end_score_ins = transition_font.render('Score: ' + str(score), True, (255, 255, 255))
+        if (len(levels) > level_idx):
+            press_key_ins = transition_font.render('Press SPACEBAR to procceed to the next level', True, (255, 255, 255))
+        else:
+            press_key_ins = transition_font.render('Press SPACEBAR to close the game', True, (255, 255, 255))
+        screen.blit(level_completed_ins, level_completed_ins.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 40)))
+        screen.blit(end_score_ins, end_score_ins.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)))
+        screen.blit(press_key_ins, press_key_ins.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 40)))
+        pygame.display.update()
